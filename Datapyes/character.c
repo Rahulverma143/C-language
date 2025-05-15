1e3ff7cb372c6a62273ab4b6edc9124ce1a97d82
Rahulverma143 <rahulvearma17@gmail.com>
2025-05-16 02:17:20 +0530

    DataTpyes updates

---
+++ Datapyes/character.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main() {
+    // character in ASCII Value to display
+    // ASCII American Standard Code for Information Interchange
+  char a = 65, b = 66, c = 67;
+  printf("%c\n", a);
+  printf("%c\n", b);
+  printf("%c", c);
+  return 0;
+}
