f2bf066a92f0b9639b6102d8c5f37cf5d380ace0
Rahulverma143 <rahulvearma17@gmail.com>
2025-05-18 01:08:46 +0530

    Update

---
+++ printfAssignmnet/5countriesName.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("1.India\n");
+    printf("2.China\n");
+    printf("3.Russia\n");
+    printf("4.America\n");
+    printf("5.Australia");
+    return 0;
+}
