384114479e57749ae05e002ad4fa0a1746b38d42
Rahulverma143 <rahulvearma17@gmail.com>
2025-05-18 01:03:23 +0530

    printfAssignment updates

---
+++ printfAssignmnet/5RiverName.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("1.Ganga\n ");
+    printf("2.Yamuna\n");
+    printf("3.Brahmaputra\n");
+    printf("4.Godavari\n");
+    printf("5.Krishna");
+    return 0;
+}
